2a15955d2e086449729bb677a9a03f99caafea38
LagutinGleb <96201919+LagutinGleb@users.noreply.github.com>
2021-12-22 15:38:35 +0300

    Button RestartOnGame made

--- Assets/Scripts/ButtonTryAgain.cs
+++ Assets/Scripts/ButtonTryAgain.cs
@@ -5,17 +5,9 @@ public class ButtonTryAgain : MonoBehaviour
 {
     public GameObject panelStart;
     public GameObject Player;
-    //private Controls controls;
-
-    private void Start()
-    {
-      //  controls = Player.GetComponent<Controls>();
-    }
 
     public void ButtonTryAgainn()
     {
-       // controls.enabled = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        
     }
 }
--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -4,9 +4,4 @@ public class Food : MonoBehaviour
 {
     public int HP;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        AudioSource sound = GetComponent<AudioSource>();
-        sound.Play();
-    }
 }
